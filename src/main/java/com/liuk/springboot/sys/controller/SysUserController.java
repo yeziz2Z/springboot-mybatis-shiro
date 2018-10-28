dec746e136fb292ab81b23e0b3fb4a9f37b7a366
liuuk <yezi12237@163.com>
2018-10-28 20:59:00 +0800

    更新

--- src/main/java/com/liuk/springboot/sys/controller/SysUserController.java
+++ src/main/java/com/liuk/springboot/sys/controller/SysUserController.java
@@ -1,20 +1,22 @@
 package com.liuk.springboot.sys.controller;
 
+import com.baomidou.mybatisplus.plugins.Page;
+import com.liuk.springboot.core.web.BaseController;
 import com.liuk.springboot.sys.entity.User;
 import com.liuk.springboot.sys.service.IUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("sys")
-public class SysUserController {
+public class SysUserController extends BaseController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -27,10 +29,12 @@ public class SysUserController {
         return "html/sys/pages_user";
     }
 
-    @GetMapping("userList")
+    @PostMapping("userList")
     @ResponseBody
-    public Object queryUserList(){
-        List<User> users = userService.selectList(null);
-        return users;
+    public Map<String, Object> queryUserList(@RequestParam("iDisplayStart") Integer start,
+                                             @RequestParam("iDisplayLength")Integer length){
+        Page<User> page = new Page<>(start / length + 1,length);
+
+        return getDataTableData(userService.selectPage(page) );
     }
 }

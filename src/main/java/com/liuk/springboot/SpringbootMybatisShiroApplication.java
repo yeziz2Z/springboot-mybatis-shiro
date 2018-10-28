e041654dceae9d5b343ede9a87415c74c8a1e3b9
liuuk <yezi12237@163.com>
2018-10-28 20:59:59 +0800

    commit

--- src/main/java/com/liuk/springboot/SpringbootMybatisShiroApplication.java
+++ src/main/java/com/liuk/springboot/SpringbootMybatisShiroApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.liuk.springboot.mapper")
+@MapperScan({"com.liuk.springboot.mapper","com.liuk.springboot.sys.mapper"})
 public class SpringbootMybatisShiroApplication {
 
 	public static void main(String[] args) {

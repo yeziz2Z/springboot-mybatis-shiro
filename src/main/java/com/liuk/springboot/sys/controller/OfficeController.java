55e384c427073899bfe845b4c0bc128145c24c5f
liuuk <yezi12237@163.com>
2018-11-20 22:40:10 +0800

    添加tree

--- src/main/java/com/liuk/springboot/sys/controller/OfficeController.java
+++ src/main/java/com/liuk/springboot/sys/controller/OfficeController.java
@@ -1,8 +1,15 @@
 package com.liuk.springboot.sys.controller;
 
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.liuk.springboot.sys.entity.Office;
+import com.liuk.springboot.sys.service.IOfficeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,12 +20,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @since 2018-07-05
  */
 @Controller
-@RequestMapping("sys")
+@RequestMapping("sys/office")
 public class OfficeController {
 
-    @RequestMapping("office")
+    @Autowired
+    IOfficeService officeService;
+
+    @RequestMapping("toOfficePage")
     public String toUser(){
         return "html/sys/pages_office";
+    }
+
+    @RequestMapping("getChildren")
+    @ResponseBody
+    public List<Office> getChildren(String parentId){
+
+        if ("-1".equals(parentId)) {
+            parentId = "0";
+        }
+        Office office = new Office();
+        office.setParentId(parentId);
+        EntityWrapper wrapper = new EntityWrapper<Office>();
+        wrapper.setEntity(office);
+        List list = officeService.selectList(wrapper);
+        return list;
     }
 }
 
